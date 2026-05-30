eb6fd6ad7cb23365d420f8277470ec64d7fce87b
isaac200111 <2425404388@live.gctu.edu.gh>
2026-05-30 15:22:37 +0000

    Add files via upload

---
+++ Isaac Kwarteng.py
@@ -0,0 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assigment Number:2
+#
+# name: Isaac Kwabena Kwarteng
+# Student ID: 2425404388
+# Email: 2425404388@live.gctu.edu.gh
+# Grade: Augustus
+#
+# On my honor, Isaac Owusu Kwarteng,this programming assigment is my own work
+# and I have  not provided this code to any other student.
+
+
+def main():
+    # prints my initials in small and large stylized letters.
+    print()
+    print("...IKK")
+    print()
+print("...IIIIIIIIIIII...KKK......KKK...KKK......KKK...***")
+print("......III........KKK....KKK.....KKK....KKK....****")
+print("......III........KKK..KKK.......KKK..KKK......****")
+print("......III........KKKKKK.........KKKKKK........****")
+print("......III........KKKKKK.........KKKKKK........****")
+print("......III........KKKKKK.........KKKKKK........****")
+print("......III........KKK..KKK.......KKK..KKK......****")
+print("......III........KKK....KKK.....KKK....KKK....****")
+print("......III........KKK......KKK...KKK......KKK..****")
+print("...IIIIIIIIIIII..KKK........KKK.KKK........KKK.****")
+
+print()
+
+main()
